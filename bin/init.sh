eb765a67dfc73bfae0419aee728e37327522e9e2
nickayson <aysonnicholas@gmail.com>
2022-12-01 15:41:39 -0800

    init leaderboard

--- bin/init.sh
+++ bin/init.sh
@@ -4,5 +4,7 @@ sqlite3 ./var/primary/mount/games.db < share/games.sql
 
 sqlite3 ./var/users.db < share/users.sql
 
+sqlite3 ./var/leaderboard.db < share/leaderboard.sql
+
 
 # ./var > mkdir primary secondary third 
