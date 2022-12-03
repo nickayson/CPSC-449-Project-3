3dc08b69e1ae7b3e53d82ddbc9dd545b39d9941a
nickayson <aysonnicholas@gmail.com>
2022-12-02 22:07:17 -0800

    fixing init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -4,7 +4,4 @@ sqlite3 ./var/primary/mount/games.db < share/games.sql
 
 sqlite3 ./var/users.db < share/users.sql
 
-sqlite3 ./var/leaderboard.db < share/leaderboard.sql
-
-
 # ./var > mkdir primary secondary third 
